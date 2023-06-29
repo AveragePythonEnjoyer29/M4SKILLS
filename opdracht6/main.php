9b06c7b710550a017f97d2ea8672ddc42ae2670e
Oscar <115230428+AveragePythonEnjoyer29@users.noreply.github.com>
2023-06-29 13:03:45 +0200

    Update main.php
    klein probleempje gefixt

--- opdracht6/main.php
+++ opdracht6/main.php
@@ -2,7 +2,7 @@
 function fillArray(int $length=10): array {
     $array = array();
     for ($i=0; $i < $length;$i++) {
-        $array[i] = i+1;
+        $array[i] = $i+1;
     }
 
     return $array
